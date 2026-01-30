681e8169d5ef390341b9657955ce76bcefb4ef5a
Viktor Sharha <vsharha07@gmail.com>
2026-01-30 10:12:54 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from pathlib import Path
+
+def download_pdfs(url: str) -> None:
+    out = Path("pdfs")
+    out.mkdir(exist_ok=True)
+
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    for a in soup.select("a[href$='.pdf']"):
+        pdf_url = urljoin(url, a["href"])
+        name = pdf_url.split("/")[-1]
+        data = requests.get(pdf_url).content
+        (out / name).write_bytes(data)
+
+def main():
+    url: str = str(input("Enter url > "))
+
+    download_pdfs(url)
+
+if __name__ == "__main__":
+    main()
